b3221ad43e22925852ffdac81697638752382e1d
ozy8 <owzhiyin@Zhiyin-Ows-MacBook-Air.local>
2016-03-28 12:25:49 +0800

    changed User.rb file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   has_many :reviews
-end
 
-def full_name user
-  return self.first_name + " " + self.last_name
+
+  def full_name user
+    return self.first_name + " " + self.last_name
+  end
+
 end
