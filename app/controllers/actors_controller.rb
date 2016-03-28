ff4776a912c772808dbc2a18174423cff2583d59
ozy8 <owzhiyin@Zhiyin-Ows-MacBook-Air.local>
2016-03-28 12:23:04 +0800

    added stuff to movies and actors controller

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,3 +1,29 @@
 class ActorsController < ApplicationController
+  def show
+    @actor = Actor.new
+  end
+
+  def new
+    @actor = Actor.new
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+
+  private
+
+  def actor_params
+      params.require(:actor).permit(:name, :year_of_birth, :thumbnail)
+  end
 
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,34 @@
 class MoviesController < ApplicationController
 
+  def index
+      @movies = Movie.all
+  end
+
+  def show
+      @movie = Movie.new
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+
+  private
+
+  def movie_params
+      params.require(:movie).permit(:title, :summary, :youtube_embeded_id, :thumbnail)
+  end
+
 end
