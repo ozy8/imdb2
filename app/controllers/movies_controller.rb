4f73809246928b370f57f8a7e3b11128ea56711f
KT Khoo <khoodose@gmail.com>
2016-03-28 12:23:31 +0800

    start to edit controllers

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,29 @@
 class MoviesController < ApplicationController
 
+  def index
+  end
+
+  def show
+    @movies = Movie.all
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+private
+
+  def movie_params
+    params.require(:movie).permit(:title, :summary, :youtube_embeded_id, :thumbnail)
+  end
+
 end
